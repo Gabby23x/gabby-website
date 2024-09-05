8b1c9cbdc5f03f5ad1ca0b067b8f502016bafb4a
Gabby <138426880+Gabby23x@users.noreply.github.com>
2024-09-05 18:52:00 -0400

    Implementing Home Page -Implemented animations -Picture -Description

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
             <ul className="navbar-list">
                 <li><a href="#home" className="navbar-link">Home</a></li>
                 <li><a href="#about" className="navbar-link">About</a></li>
-                <li><a href="#services" className="navbar-link">Services</a></li>
+                <li><a href="#services" className="navbar-link">Projects</a></li>
                 <li><a href="#contact" className="navbar-link">Contact</a></li>
             </ul>
         </nav>
--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,26 @@ import Image from "next/image";
 import Navbar from "./navbar";
 import "./home.css"; // Import the CSS file
 
+
 export default function Home() {
   return (
     <div className="container">
       <Navbar /> {/* Add the Navbar component here */}
-      <h1 className="title">Gabby</h1>
-      <div className="image-placeholder">
-        <span>Image</span>
+      <div className="content">
+        <div className="text-container">
+          <p className="description animationt" >Hello I am</p>
+          <h1 className="title">Gabrielle Loncke (Gabby)</h1>
+          <p className="description typing-effect">
+            A passionate CS and Game Dev Student<br />
+            with a love for creating amazing
+            digital experiences.
+          </p>
+        </div>
+        <div className="image-container">
+          <div className="image-circle">
+            <Image src="/images/profile.jpg" alt="Profile Picture" width={5472} height={3648} />
+          </div>
+        </div>
       </div>
     </div>
   );
